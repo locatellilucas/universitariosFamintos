c60a4f7151bdd8610a99307976a1943b003763fc
Lucas Locatelli <locatellilucas11@gmail.com>
2016-06-27 17:23:34 -0300

    final_cart

--- universitariosFamintos/CarrinhoViewController.swift
+++ universitariosFamintos/CarrinhoViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class CarrinhoViewController: UIViewController {
+class Teste{
+    var nome: String
+    var info: String
+    var qtde: String
+    var preco: String
+
+    init(nome: String, info: String, qtde: String, preco: String){
+        self.nome = nome
+        self.info = info
+        self.qtde = qtde
+        self.preco = preco
+        
+    }
+}
+
+class CarrinhoViewController: UIViewController, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var tableViewCart: UITableView!
+    
+    var testes: [Teste] = [Teste]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableViewCart.dataSource = self
+        let teste1 = Teste(nome: "Pizza", info: "Frango com catupery", qtde: "2", preco: "R$5,00")
+        
+        self.testes.append(teste1)
+        self.testes.append(teste1)
+        self.testes.append(teste1)
+        self.testes.append(teste1)
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +49,36 @@ class CarrinhoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.testes.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CartIdentifier", forIndexPath: indexPath) as! ItensCartViewCell
+        
+        //indexPath.section
+        //indexPath.row
+        
+        let teste = self.testes[indexPath.row]
+        
+        cell.nomePratoCarrinho.text = teste.nome
+        cell.InfoPratoCarrinho.text = teste.info
+        cell.qtdePratoCarrinho.text = teste.qtde
+        cell.precoPratoCarrinho.text = teste.preco
+        
+        return cell
+    }
+    
+
 
     /*
     // MARK: - Navigation
