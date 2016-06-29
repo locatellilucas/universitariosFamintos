e5ce308a228ed2ac63abe5cb3728e3a8b5230daa
JonesMans <jonesmansur@gmail.com>
2016-06-29 15:46:05 -0300

    Restaurantes

---
+++ universitariosFamintos/RestaurantesTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  RestaurantesTableViewCell.swift
+//  universitariosFamintos
+//
+//  Created by Jones Baglioli on 6/29/16.
+//  Copyright © 2016 Chamer. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantesTableViewCell: UITableViewCell {
+
+    
+  
+    @IBOutlet weak var restauranteImage: UIImageView!
+    
+    @IBOutlet weak var restauranteNomeLabel: UILabel!
+
+    @IBOutlet weak var restauranteTipoLabel: UILabel!
+    
+}
